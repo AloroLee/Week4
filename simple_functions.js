2cc40c85180c41ba49bf6990ce0f566f2630899d
aloro lee <alorotams@gmail.com>
2024-10-15 10:39:55 -0400

    exercise

---
+++ simple_functions.js
@@ -0,0 +1,10 @@
+function favoritethings(){
+    console.log("favorite movie: pof")
+
+}
+favoritethings ();
+
+function myname(name){
+    console.log(`My firstname is ${name}`);
+}
+myname ("lee")
