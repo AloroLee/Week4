f27287747f7e28700775ff88331203a4b8c4f2b4
aloro lee <alorotams@gmail.com>
2024-10-15 13:28:23 -0400

    string index

---
+++ string_index.js
@@ -0,0 +1,14 @@
+//           111111111122222222222333333333
+// 0123456789012345678901234567890123456789
+text = "Now is the time for all good people..."
+index = text.indexOf(" ")
+console.log(index)
+console.log(text.length)
+firstDot = text.indexOf(".")
+console.log(firstDot)
+
+nPosition = text.indexOf("N")
+console.log(nPosition)
+
+zPosition = text.indexOf("z")
+console.log(zPosition) // -1 not found
