1e2ada1ece2b506ba1ee975c039c8bdd9f8c477f
aloro lee <alorotams@gmail.com>
2024-10-16 13:26:18 -0400

    parce repeating the same lime

---
+++ parse.js
@@ -0,0 +1,21 @@
+
+// PARSE - cutting things into pieces, parcel
+
+// body of text is a corpus
+
+corpus = "Now is the time..."
+
+// console.log(corpus[0])
+// console.log(corpus[1])
+// console.log(corpus[2])
+// console.log(corpus[3])
+// console.log(corpus[4])
+
+// i is for index
+// i++ is shorthand for i = i + i
+
+for(i=0; i < corpus.length; i++){
+    console.log(corpus[i])
+}
+
+console.log(corpus.length)
