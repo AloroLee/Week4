a55c54104e90d5c9f5148f58ceaf6b68461b9283
aloro lee <alorotams@gmail.com>
2024-10-16 09:45:49 -0400

    updates

--- more_functions.js
+++ more_functions.js
@@ -20,3 +20,29 @@ displaymailinglabel("423 longhorn road")
 displaymailinglabel("lynchburg")
 displaymailinglabel("Virginia")
 displaymailinglabel("40467")
+
+// function add(a,b){
+//     let result = a + b;
+//     console.log(result)
+// }
+// add(6,7)
+
+function add(a,b){
+    let result = a + b;
+    return result;
+}
+const result = add(6,7)
+console.log(result)
+
+function getReceipt (amountPaid, changeDue){
+    const total = 100;
+    // const text = amountPaid + ", " + changeDue
+   const text = `
+Amount PAID: ${amountPaid}
+CHANGE DUE: ${changeDue}
+TOTAL: ${total}
+   `;
+    return text;
+}
+
+console.log(getReceipt(10,3))
