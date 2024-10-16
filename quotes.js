842d8330a8b01594143ce44f19c760599415cd18
aloro lee <alorotams@gmail.com>
2024-10-16 11:18:29 -0400

    New day quotes

---
+++ quotes.js
@@ -0,0 +1,15 @@
+
+text1 = "now is time..." //double-Quote
+text2 = 'now is time...' //single-quote
+text3 = `now is time...` //backtick
+
+lastName = "O'Reilly"
+
+phrase1 = "They said \"Hello\"" //ESCAPE with\
+phrase2 = 'They said "Hello"'
+
+phrase3 = 'O\'Reilly said "Hello"' //ESCAPE with \
+
+
+// Modern template backtick
+phrase4 = `O'Reilly said "Hello"`
